a72b409c61186c26bf6331c48d577fbbe49c21fd
kimsohee <koreait.sec2020@gmail.com>
2025-08-13 18:01:09 +0900

    Mybatis-spring

---
+++ spring_3mybatis/src/main/java/org/iclass/spring_3mybatis/mapper/BuyMapper.java
@@ -0,0 +1,25 @@
+package org.iclass.spring_3mybatis.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.iclass.spring_3mybatis.dto.BuyDto;
+import org.iclass.spring_3mybatis.dto.CustomerBuyDto;
+
+public interface BuyMapper {
+  List<BuyDto> selectByCustomer(String customer_id);
+
+  List<BuyDto> selectByPcode(String pcode);
+
+  List<BuyDto> selectByYear(String year);
+
+  int selectSumByPcode(String pcode);
+
+  // join
+  List<CustomerBuyDto> selectSaleByCustomer(String customer_id);
+
+  // count 집계 함수
+  Map<String, Integer> selectCountByYear(String year);
+
+  List<Map<String, Object>> selectAllCountByYear();
+}
