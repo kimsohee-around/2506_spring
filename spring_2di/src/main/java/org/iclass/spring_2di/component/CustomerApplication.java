750437f70e019a9731aaef7302d05d012b3eb5c2
kimsohee <koreait.sec2020@gmail.com>
2025-08-13 15:25:27 +0900

    setter 주입

---
+++ spring_2di/src/main/java/org/iclass/spring_2di/component/CustomerApplication.java
@@ -0,0 +1,24 @@
+package org.iclass.spring_2di.component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class CustomerApplication {
+
+  public static void main(String[] args) {
+    ApplicationContext context = SpringApplication.run(CustomerApplication.class, args);
+
+    log.info("-----------------------");
+    CustomerDao dao = context.getBean(CustomerDao.class);
+    dao.setGroups();
+    CustomerService service = context.getBean(CustomerService.class);
+    service.test();
+    CustomerController controller = context.getBean(CustomerController.class);
+    controller.test();
+  }
+}
--- spring_2di/src/main/java/org/iclass/spring_2di/component/CustomerService.java
+++ spring_2di/src/main/java/org/iclass/spring_2di/component/CustomerService.java
@@ -10,13 +10,19 @@ import lombok.extern.slf4j.Slf4j;
 public class CustomerService {
   private CustomerDao dao;
 
+  // => 자동 주입 없을 때 NullPointerException 확인하기 : setter 자동 실행하지 않고, dao bean 가져오기 X
   @Autowired
   public void setDao(CustomerDao dao) {
     this.dao = dao;
+    log.info("setDao 실행 완료!!");
   }
 
   public void test() {
+    dao.setGroups(); // dao 가 null 이 아니면 정상실행
     log.info("CustomerService test - dao : {}", this.dao.getClass().toString());
-    dao.setGroups();
+  }
+
+  public void print() {
+    log.info("service message : HI~");
   }
 }
