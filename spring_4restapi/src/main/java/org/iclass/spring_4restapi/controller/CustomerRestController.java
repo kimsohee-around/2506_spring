aee0f3d9a0fcfc8222547108f00fe0bf0d4c59a3
kimsohee <koreait.sec2020@gmail.com>
2025-08-14 16:53:32 +0900

    rest api

--- spring_4restapi/src/main/java/org/iclass/spring_4restapi/controller/CustomerRestController.java
+++ spring_4restapi/src/main/java/org/iclass/spring_4restapi/controller/CustomerRestController.java
@@ -1,11 +1,16 @@
 package org.iclass.spring_4restapi.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.iclass.spring_4restapi.Spring4RestapiApplication;
 import org.iclass.spring_4restapi.dto.CustomerDto;
 import org.iclass.spring_4restapi.service.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,13 +44,15 @@ public class CustomerRestController {
 
   @PostMapping("/api/customers")
   public ResponseEntity<?> save(@RequestBody CustomerDto dto) {
+    // @RequestBody : 요청의 본문 json 문자열을 자바 CustomerDto 타입 객체로 변환
+    // ㄴ 여러개의 값을 dto 타입으로 받을 때 필요.
     // insert 는 db에서 무결성 위반 등 오류 발생 가능성 있음.
     try {
       int result = service.join(dto);
-      return ResponseEntity.ok().body(result);
+      return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("success", result));
     } catch (Exception e) {
       log.info("save 예외 : {}", e.getMessage());
-      return ResponseEntity.badRequest().build(); // 응답 body X
+      return ResponseEntity.badRequest().body(Map.of("error", e.getMessage())); // 응답 body X
     }
 
   }
