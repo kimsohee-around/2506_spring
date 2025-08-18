71679d9827feb73948af2f811a0cee4b9fb9a06d
kimsohee <koreait.sec2020@gmail.com>
2025-08-18 16:04:33 +0900

    web mvc 파라미터

---
+++ spring_5webmvc/src/main/java/org/iclass/spring_5webmvc/controller/TestController_2Parameter.java
@@ -0,0 +1,29 @@
+package org.iclass.spring_5webmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class TestController_2Parameter {
+  // 파라미터 : 쿼리문자열 or form 요소의 값
+
+  @GetMapping("/list")
+  public String list(String name, @RequestParam(defaultValue = "0") int age) { // 메소드의 인자는 변수명과 같은 이름 파라미터를 저장.
+
+    log.info("name :  {}", name);
+    log.info("age :  {}", age);
+    return "list"; // list.html
+  }
+
+  // 파라미터 4개 받기 : name, age, address, gender(String)
+  @GetMapping("/listA")
+  public String listA(String name, int age, String address, String gender) {
+
+    log.info("파라미터 : {} {} {} {}", name, age, address, gender);
+    return "list"; // list.html
+  }
+}
