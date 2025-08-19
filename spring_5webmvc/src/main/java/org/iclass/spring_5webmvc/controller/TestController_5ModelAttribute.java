a02c2228c22cfdd75341bf354a4948625557a360
kimsohee <koreait.sec2020@gmail.com>
2025-08-19 15:25:53 +0900

    모델 애트리뷰트

--- spring_5webmvc/src/main/java/org/iclass/spring_5webmvc/controller/TestController_5ModelAttribute.java
+++ spring_5webmvc/src/main/java/org/iclass/spring_5webmvc/controller/TestController_5ModelAttribute.java
@@ -1,10 +1,12 @@
 package org.iclass.spring_5webmvc.controller;
 
+import org.iclass.spring_3mybatis.dto.ProductDto;
 import org.iclass.spring_3mybatis.mapper.ProductMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.AllArgsConstructor;
@@ -19,9 +21,18 @@ public class TestController_5ModelAttribute {
 
   @GetMapping("/product/search")
   public String search(@ModelAttribute(name = "keyword") String keyword,
-      @ModelAttribute(name = "page", value = "1") int page) {
+      @ModelAttribute(name = "page") String page) {
     log.info("파라미터 : {} , {}", keyword, page);
+    log.info("product/search.html 로 2개의 값을 keyword,page 애트리뷰 이름으로 전달합니다.");
+    return "product/search";
+  }
+  // @ModelAttribute 는 단일 파라미터에서는 잘 사용하지 ❌, 주로 여러개 필드를 묶은 dto에서 사용
 
+  @GetMapping("/product/search2")
+  public String search2(@ModelAttribute ProductDto dto) {
+    // 기본 애트리뷰트 이름은 productDto, 이름 변경은 name = "dto" 와 같이 합니다.
+    log.info("파라미터 dto : {}", dto);
+    log.info("product/search.html 로 dto 에 저장된 파라미터 값이 search.html 에 보이는지 확인하기!! ");
     return "product/search";
   }
 
@@ -30,6 +41,12 @@ public class TestController_5ModelAttribute {
   public String prod_list(Model model) {
     // 테이블에서 모든 행을 조회하여 애트리뷰트 저장&view 로 전달
     model.addAttribute("list", productMapper.selectAll());
+    return "product/list";
+  }
+
+  @PostMapping("/product/list")
+  public String prod_search(@ModelAttribute String keyword) {
+
     return "product/list";
   }
 
