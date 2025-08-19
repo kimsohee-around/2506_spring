35c9627e6d921fe2d78f0c742b92e99ddfb174a2
kimsohee <koreait.sec2020@gmail.com>
2025-08-19 12:11:38 +0900

    애트리뷰트

--- spring_5webmvc/src/main/java/org/iclass/spring_3mybatis/dto/CustomerDto.java
+++ spring_5webmvc/src/main/java/org/iclass/spring_3mybatis/dto/CustomerDto.java
@@ -20,5 +20,10 @@ public class CustomerDto {
     private final Integer age; // int 로 하면 안되는 경우 : null 값이 있을 때(int 에 저장 못합니다.)
     private final Date regDate;
 }
-// 과거 마이바티스 3.4 이전 버전은 getter,setter,기본생성자 없으면 오류 발생.
-// -> 현재 버전은 문제 없이 정상 실행.
+/*
+ * 자바의 날짜 타입은
+ * 자바 8 버전 이전 : java.util.Date
+ * -> 타임리프 날짜 출력 기능의 객체를 #dates 유틸리티로 사용
+ * 자바 8 버전 이후 : java.time.LocalDate, LocalTime
+ * -> 타임리프 날짜 출력 기능의 객체를 #temporals 유틸리티로 사용
+ */
