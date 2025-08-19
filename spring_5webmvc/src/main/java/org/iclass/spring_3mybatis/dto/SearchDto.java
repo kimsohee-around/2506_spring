e487036b420f20a36670cb245f12ed8bcec2b328
kimsohee <koreait.sec2020@gmail.com>
2025-08-19 16:04:31 +0900

    상품검색-동적쿼리

---
+++ spring_5webmvc/src/main/java/org/iclass/spring_3mybatis/dto/SearchDto.java
@@ -0,0 +1,17 @@
+package org.iclass.spring_3mybatis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SearchDto {
+  // 검색 그룹안에 있는 항목으로 필드 정의
+  private String pname;
+  private String pcode;
+  private Integer minPrice;
+  private Integer maxPrice;
+
+}
