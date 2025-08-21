3e1476dc67811db8ededd15d2fde0a75860a31bf
kimsohee <koreait.sec2020@gmail.com>
2025-08-21 09:13:53 +0900

    게시판-step 2 메인글

---
+++ spring_6board/src/main/java/org/iclass/board/controller/CommunityController.java
@@ -0,0 +1,83 @@
+package org.iclass.board.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.iclass.board.dto.CommunityDTO;
+import org.iclass.board.service.CommunityService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@AllArgsConstructor
+@Slf4j
+@Controller
+public class CommunityController {
+	private CommunityService service;
+
+	@GetMapping("/community/list")
+	public String list(Model model) {
+
+		List<CommunityDTO> list = service.selectAll();
+		model.addAttribute("list", list);
+
+		log.info("오늘 날짜 : {}", LocalDate.now());
+		model.addAttribute("today", LocalDate.now());
+		return "community/list"; // community 폴더안에 list.html
+	}
+
+	// 글 읽기
+	@GetMapping("/community/read")
+	public String read(int idx, Model model) {
+		// ㄴ list.html 화면에서 글제목 링크에 page 파라미터 받아오기
+		log.info("idx : {} ", idx);
+		// sql : idx 값으로 하나의 행 조회, mapper, service
+		model.addAttribute("dto", service.read(idx, true));
+		return "community/read";
+	}
+
+	// 글 쓰기
+	@GetMapping("/community/write")
+	public String write() {
+
+		return "community/write";
+	}
+
+	@PostMapping("/community/write")
+	public String write(CommunityDTO dto) {
+		log.info("form 입력값 : {}", dto);
+		service.write(dto);
+		return "redirect:list";
+	}
+
+	// 글 수정
+	@GetMapping("/community/modify")
+	public String modify(int idx, Model model) {
+		CommunityDTO dto = service.read(idx, false); // 글 수정은 조회수 카운트 안함
+		model.addAttribute("dto", dto);
+		return "community/modify"; // write.html 활용
+	}
+
+	// 수정할 내용 저장
+	@PostMapping("/community/modify")
+	public String modify(CommunityDTO dto, RedirectAttributes reAttr) {
+		log.info("modify dto : {}", dto);
+		service.save(dto);
+		reAttr.addAttribute("idx", dto.getIdx());
+
+		return "redirect:read";
+	}
+
+	// 글 삭제
+	@GetMapping("/community/remove")
+	public String remove(int idx, RedirectAttributes reAttr) {
+		service.remove(idx);
+		return "redirect:list";
+	}
+
+}
