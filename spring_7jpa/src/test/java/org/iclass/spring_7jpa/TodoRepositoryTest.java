16f840f8b8ff8608a23437c4aeaab636d72a3519
kimsohee <koreait.sec2020@gmail.com>
2025-08-25 14:55:13 +0900

    jpa

--- spring_7jpa/src/test/java/org/iclass/spring_7jpa/TodoRepositoryTest.java
+++ spring_7jpa/src/test/java/org/iclass/spring_7jpa/TodoRepositoryTest.java
@@ -3,6 +3,9 @@ package org.iclass.spring_7jpa;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import org.iclass.spring_7jpa.entity.TodoEntity;
 import org.iclass.spring_7jpa.repository.TodoRepository;
@@ -18,6 +21,27 @@ public class TodoRepositoryTest {
 
   @Autowired
   private TodoRepository todoRepository;
+
+  @Test
+  void saveDummies() {
+    String[] names = { "himedia", "iclass" };
+    String[] todos = { "청소", "운동", "영어공부", "회의" };
+    List<TodoEntity> list = new ArrayList<>();
+    // for(int i=1;i<10;i++)
+    LocalDate baseTime = LocalDate.of(2025, 8, 1);
+    IntStream.rangeClosed(1, 20).forEach(i -> {
+      TodoEntity entity = TodoEntity.builder()
+          .title(todos[i % 4])
+          .username(names[i % 2])
+          .todo_date(baseTime.plusDays(i))
+          .checked(i % 2 == 0) // 참이면 1, 거짓이면 0
+          .build();
+      list.add(entity);
+    });
+    todoRepository.saveAll(list);
+    assertEquals(21, todoRepository.count());
+    // count()메소드: select count() from 테이블;
+  }
 
   @Test
   void saveOne() {
